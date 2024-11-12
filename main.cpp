1c10aa8be5e809acf834853d04dcf710bd9e37c9
CorinaMateescu <mateescu.corina.w6k@student.ucv.ro>
2024-11-12 08:51:48 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <vector>
+#include <unordered_set>
 using namespace std;
 
 int main()
+{
+    ifstream inFile("input3.txt");
+
+    int no_problems, no_doctors;
+    string name, speciality;
+
+    inFile >> no_problems;
+    vector<string>necessary;//vector pt specializarile de care avem nevoie
+    for (int i = 0; i < no_problems; i++)
+    {
+        inFile >> name;
+        inFile >> speciality;
+        necessary.emplace_back(speciality);
+        //cout << name << ' ' << speciality << '\n';
+    }
+
+    inFile >> no_doctors;
+    unordered_set<string>doctors;// set in care salvam specializarile doctorilor
+    for (int i = 0; i < no_doctors; i++)
+    {
+        inFile >> name;
+        inFile >> speciality;
+        doctors.insert(speciality);
+        // cout << name << ' ' << speciality << '\n';
+    }
+    inFile.close();
+    for (const string& speciality : necessary)//specializarile din vectorul necessary
+    {
+        if (doctors.find(speciality) != doctors.end())// daca gasim specializarea pana la finalul set-ului scriem admis
+        {
+            cout << speciality << " Acceptat"<<endl;
+        }
+        else
+        {
+            cout << speciality << " Respins"<<endl;//altfel respins
+        }
+    }
+    return 0;
+}
+
+/*int main()
 {
     ifstream inFile("input.txt");
 
@@ -12,12 +54,13 @@ int main()
     string name, speciality;
     
     inFile >> no_problems;
-
+    vector<string>necessary;
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        necessary.emplace_back(speciality);
+        //cout << name << ' ' << speciality << '\n';
     }
 
     inFile >> no_doctors;
@@ -26,8 +69,15 @@ int main()
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        for (int i = 0; i < no_doctors; i++)
+        {
+            if (necessary[i] == speciality)
+                cout << speciality<<" " << "Accepted"<<endl;
+            else
+                cout << speciality << " " << "Rejected" << endl;
+        }
+       // cout << name << ' ' << speciality << '\n';
     }
-
+   
     return 0;
-}
+}*/
